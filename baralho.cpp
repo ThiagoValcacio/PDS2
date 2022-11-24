fdd80ce4c5438688c3f91f0d1caf0a8355208e05
arthurbessa <97554260+arthurbessa@users.noreply.github.com>
2022-11-24 10:29:31 -0300

    Update baralho.cpp

--- baralho.cpp
+++ baralho.cpp
@@ -5,23 +5,80 @@ using namespace carta_truco;
 
 baralho::baralho(){
  std::vector<Naipe>vec_naipes{Copas,Ouros, Espadas, Paus};
- std::vector<Numero>vec_numero{Quatro, Cinco, Seis, Sete, Valete, Dama, Rei, As, Dois, Tres };
+ std::vector<Numero>vec_numero{Tres, Dois, As, Rei, Valete, Dama, Sete, Seis, Cinco, Quatro};
  std::vector<Carta>_deck;
 
- for(auto i:vec_naipes){
+//Inserindo Manilhas em ordem 
+ _deck.push_back(Carta(Quatro, Paus));
+ _deck.push_back(Carta(Sete, Copas));
+ _deck.push_back(Carta(As, Espadas));
+ _deck.push_back(Carta(Sete, Ouros));
+
+
     for(auto j: vec_numero){
-        _deck.push_back(Carta(i, j));
+       
+        if(j == As){
+            _deck.push_back(Carta(j, Copas));
+            _deck.push_back(Carta(j, Ouros));
+            _deck.push_back(Carta(j, Paus));
+
+        } else if(j == Quatro){
+            _deck.push_back(Carta(j, Ouros));
+            _deck.push_back(Carta(j, Copas));
+            _deck.push_back(Carta(j, Espadas));
+        }else if(j == Sete){
+            _deck.push_back(Carta(j, Paus));
+            _deck.push_back(Carta(j, Espadas));
+        } else  for(auto k: vec_naipes){
+            _deck.push_back(Carta(j, k));
+        }
     }
- }
+ 
 }
 
-void baralho::exclui_cartas(Carta carta){
- for(int i=0; i<_deck.size();i++){
-    if(carta.get_nome() == _deck[i].get_nome()){
-        _deck.erase(_deck.begin() + (i-1));
-    }
- }
-
+std::string baralho::E_maior(Carta carta1, Carta carta2){
+    int parametro1 = 0;
+    int parametro2 = 0;
+    for(int i=0; i<_deck.size();i++){
+        
+        if(_deck[i].get_nome() == carta1.get_nome()){
+                parametro1 = i;
+            }
+            if(_deck[i].get_nome() == carta2.get_nome()){
+                parametro2 = i;
+            }
+        }
+        if(carta1.get_numero() != carta2.get_numero()){
+            if (parametro1 < parametro2){
+                return "Maior";
+            } if (parametro1 > parametro2){
+                return "Menor";
+            }
+        } else if (carta1.get_numero() != Quatro || Sete || As && carta2.get_numero() != Quatro || Sete || As){
+            return "Igual";
+        } else if (carta1.get_numero() == Quatro && carta1.get_naipe() == Paus){
+            return "Maior";
+        } else if (carta2.get_numero() == Quatro && carta2.get_naipe() == Paus){
+            return "Menor";
+        }else return "Igual";
+        if(carta1.get_numero() == carta2.get_numero()){
+        if (carta1.get_numero() == As && carta1.get_naipe() == Espadas){
+            return "Maior";
+        } else if (carta2.get_numero() == As && carta2.get_naipe() == Espadas){
+            return "Menor";
+        } else return "Igual";
+        }
+        if(carta1.get_numero() == carta2.get_numero()){
+        if (carta1.get_numero() == Sete && carta1.get_naipe() == Copas){
+            return "Maior";
+        } else if (carta2.get_numero() == Sete && carta2.get_naipe() == Copas){
+            return "Menor";
+        } else if (carta1.get_numero() == Sete && carta1.get_naipe() == Ouros){
+            return "Maior";
+        } else if (carta2.get_numero() == Sete && carta2.get_naipe() == Ouros){
+            return "Menor";
+            
+        } else return "Igual";
  
 }
 
