163938d1afe2ea8da35269e5c7a89ca882c9ada6
João Henrique Salles Rocha <joaohenrique008@gmail.com>
2022-11-27 23:54:43 -0300

    Update interface.cpp

--- interface.cpp
+++ interface.cpp
@@ -1,6 +1,13 @@
 #include "interface.hpp"
 
-void interface::interface(){
+interface::interface(){
   cout << "\n _____ _____  __  __ _____ _____ \n|_   _| __  |  |  |     |     |\n  | | |    -|  |  |   --|  |  |\n  |_| |__|__|_____|_____|_____|\n";
   cout << "Desenvolvido por João Henrique, Arthur Bessa, Thiago Valcácio, Lucas Bretas e Tasso!";
+}
+
+void mesa(vector<jogador> Jogadores, int i){
+
+
+
+
 }
