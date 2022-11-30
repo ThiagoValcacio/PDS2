4d79c902f096a275ee4039436226c78b8dd28f03
arthurbessa <97554260+arthurbessa@users.noreply.github.com>
2022-11-30 14:06:21 -0300

    Update cartas.hpp

--- cartas.hpp
+++ cartas.hpp
@@ -1,19 +1,14 @@
-#ifndef CARTAS_HPP
-#define CARTAS_HPP
-#include <string>
- namespace carta_truco{
- enum Naipe {Copas, Ouros, Espadas, Paus};
- enum Numero{Quatro, Cinco, Seis, Sete, Valete, Dama, Rei, As, Dois, Tres};
- class Carta {
- private:
- Naipe _naipe;
- Numero _numero;
- std::string _nome_carta;
- public:
- Carta(Naipe naipe, Numero numero);
- Numero get_numero() const;
- Naipe get_naipe() const;
- std::string get_nome() const;
- };
- }
-#endif
+#include <iostream>
+#include <vector> 
+#include "cartas.hpp"
+
+Carta::Carta(std::string nome, int valor) {
+ _nome = nome;
+ _valor = valor;
+}
+std::string Carta::get_nome() const {
+ return _nome;
+}
+int Carta::get_valor() const {
+ return _valor;
+}
