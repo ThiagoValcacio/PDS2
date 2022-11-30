d1b2a8453c4081197722dcb8cd3e2a243ea87a8d
lucasmbretas <112027750+lucasmbretas@users.noreply.github.com>
2022-11-30 13:17:26 -0300

    Update rodada.hpp

--- rodada.hpp
+++ rodada.hpp
@@ -1,1 +1,19 @@
-#define
+#ifndef RODADA_H
+#define RODADA_H
+
+class Rodada : public Baralho{
+
+private:
+
+vector<Cartas> carta;
+vector<Jogadores> jogador;
+
+public:
+
+get_vencedor();
+get_primeiro();
+canga();
+
+};
+
+#endif
