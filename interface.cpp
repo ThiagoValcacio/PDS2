861ed9320f5640fc90aa454c686c2e4b6c0cc173
João Henrique Salles Rocha <joaohenrique008@gmail.com>
2022-11-30 13:48:42 -0300

    Update interface.cpp

--- interface.cpp
+++ interface.cpp
@@ -5,9 +5,20 @@ interface::interface(){
   cout << "Desenvolvido por João Henrique, Arthur Bessa, Thiago Valcácio, Lucas Bretas e Tasso!";
 }
 
-void mesa(vector<jogador> Jogadores, int i){
+void interface::mesa(vector<jogador> Jogadores, int i){
 
-
-
+  
+  while(True){
+    int num_jog;
+    cout << "Quantos jogadores farão parte do jogo? (2/4)" << endl;
+    cin >> num_jog;
+    if(num_jog == 2 || num_jog == 4){
+      cout << "O jogo será iniciado com " << num_jog << " jogadores" << endl;
+      break;
+    }
+    else{
+      cout << "Argumento Inválido, Tente novamente: " << endl;
+    }
+  }
 
 }
