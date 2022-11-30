ac7a8f6e1b3a231053fdc33a0ba095939516586e
arthurbessa <97554260+arthurbessa@users.noreply.github.com>
2022-11-30 14:06:58 -0300

    Update cartas.cpp

--- cartas.cpp
+++ cartas.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
 #include <vector> 
 #include "cartas.hpp"
-using namespace carta_truco;
-Carta::Carta(Naipe naipe, Numero numero) {
- _naipe = naipe;
- _numero = numero;
- _nome_carta = std::to_string(naipe) + std::to_string(numero);
+
+Carta::Carta(std::string nome, int valor) {
+ _nome = nome;
+ _valor = valor;
 }
-Numero Carta::get_numero() const {
- return _numero;
+std::string Carta::get_nome() const {
+ return _nome;
 }
-Naipe Carta::get_naipe() const {
- return _naipe;
+int Carta::get_valor() const {
+ return _valor;
 }
-std::string Carta:: get_nome() const{
- return _nome_carta;
-}
+
 
