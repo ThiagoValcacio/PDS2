6da2e7a672426c791d6b68c19797fa2d7381d068
TassoossaT <71718028+TassoossaT@users.noreply.github.com>
2022-12-03 12:05:01 +0000

    Delete test_jogador.cpp

--- testes/test_jogador.cpp
+++
@@ -1,9 +0,0 @@
-#include "../testes/doctest-master/doctest/doctest.h" //https://github.com/doctest/doctest.git
-//#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-
-#include "../src/jogador.cpp" 
-
-TEST_CASE( "Test jogador" )
-{
-    CHECK( 1==0);
-}
