2402a61c6781ad85d6867fc5bbcf44bdf2c31d74
TassoossaT <71718028+TassoossaT@users.noreply.github.com>
2022-12-03 12:04:52 +0000

    Delete test_bot.cpp

--- testes/test_bot.cpp
+++
@@ -1,9 +0,0 @@
-#include "../testes/doctest-master/doctest/doctest.h" //https://github.com/doctest/doctest.git
-//#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-
-#include "../src/bot.cpp" 
-
-TEST_CASE( "Test bot" )
-{
-    CHECK( 1==0);
-}
